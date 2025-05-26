ad742cf6927c25c187a80518f8aad5e5b9bc84be
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2025-05-26 21:09:36 +0100

    feat: Typing functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,6 +1,5 @@
-type PillProps = {
-  title: string;
-};
+import { PillProps } from "@/interfaces";
+
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
